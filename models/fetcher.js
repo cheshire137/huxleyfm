0fdb11ad13b2373bb9f306a46d3c015615132f6f
Sarah Vessels <cheshire137@github.com>
2016-05-29 19:36:05 -0500

    Handle non-JSON responses in Fetcher

--- models/fetcher.js
+++ models/fetcher.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 module.exports = class Fetcher {
   get(url) {
     const options = { method: 'GET' };
-    return this.fetchJSON(url, options);
+    return this.makeRequest(url, options);
   }
 
   post(url, params) {
@@ -13,25 +13,43 @@ module.exports = class Fetcher {
       body.push(key + '=' + encodeURIComponent(params[key]));
     }
     const options = { method: 'POST', body: body.join('&') };
-    return this.fetchJSON(url, options);
+    return this.makeRequest(url, options);
   }
 
-  fetchJSON(url, options) {
+  makeRequest(url, options) {
     if (typeof options.headers === 'undefined') {
       options.headers = {};
     }
     options.headers['User-Agent'] = Config.user_agent;
     return new Promise((resolve, reject) => {
       fetch(url, options).then((response) => {
-        response.json().then((json) => {
-          if (response.ok) {
-            resolve(json);
-          } else {
-            json.url = url;
-            json.status = response.status + ' ' + response.statusText;
-            reject(json);
-          }
-        });
+        const respType = response.headers.get('Content-Type');
+        const status = response.status + ' ' + response.statusText;
+        if (respType === 'text/html') {
+          response.text().then((body) => {
+            if (response.ok) {
+              resolve({ body: body });
+            } else {
+              reject({ url: url, status: status, body: body });
+            }
+          }).catch((error) => {
+            console.error('failed to parse html response', error);
+            reject({ error: error });
+          });
+        } else {
+          response.json().then((json) => {
+            if (response.ok) {
+              resolve(json);
+            } else {
+              json.url = url;
+              json.status = status;
+              reject(json);
+            }
+          }).catch((error) => {
+            console.error('failed to parse json response', error);
+            reject({ error: error });
+          });
+        }
       });
     });
   }
