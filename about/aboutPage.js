a4e90cf8b408d9fa11c3f7ea7978e30baebf0318
Sarah Vessels <cheshire137@github.com>
2016-05-30 20:50:49 -0500

    Don't get SomaScrobbler version on about page load

--- about/aboutPage.js
+++ about/aboutPage.js
@@ -6,31 +6,12 @@ module.exports = class AboutPage extends Eventful {
     console.debug('about page init');
     super();
     this.findElements();
-    this.getVersionInfo();
   }
 
   findElements() {
-    this.versionInfo = document.getElementById('version-info');
   }
 
   removeListeners() {
     console.debug('unbinding about page listeners');
-  }
-
-  getVersionInfo() {
-    const soma = new Soma();
-    soma.getScrobblerApiVersion().
-         then(this.showVersionInfo.bind(this)).
-         catch(this.scrobblerVersionError.bind(this));
-  }
-
-  showVersionInfo(data) {
-    console.log(data);
-    this.versionInfo.classList.remove('hidden');
-  }
-
-  scrobblerVersionError(error) {
-    console.error('failed to fetch SomaScrobbler version', error);
-    this.emit('error', 'Error accessing SomaScrobbler API.');
   }
 }
