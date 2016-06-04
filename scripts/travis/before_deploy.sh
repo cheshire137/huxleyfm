35e572585bccd76317b5e847ff24559f0de1fb86
Sarah Vessels <cheshire137@gmail.com>
2016-06-04 17:46:50 -0500

    Modify Travis set up to split Windows, Linux builds
    Job is stalling out trying to build Windows:
    https://travis-ci.org/cheshire137/huxleyfm/jobs/135320225

--- scripts/travis/before_deploy.sh
+++ scripts/travis/before_deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-	npm run build
-	(cd dist; zip -r HuxleyFM-$TRAVIS_TAG-darwin-x64.zip HuxleyFM-darwin-x64)
+  if [[ "$WINE" == "true" ]]; then
+  	npm run-script build-osx
+  	(cd dist; zip -r HuxleyFM-$TRAVIS_TAG-darwin-x64.zip HuxleyFM-darwin-x64)
+  fi
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-	npm run-script build-linux
-	(cd dist; tar zcvf HuxleyFM-$TRAVIS_TAG-linux-ia32.tar.gz HuxleyFM-linux-ia32)
-	(cd dist; tar zcvf HuxleyFM-$TRAVIS_TAG-linux-x64.tar.gz HuxleyFM-linux-x64)
-
-	# also try windows!
-	npm run-script build-windows-wine
-	(cd dist; zip -r HuxleyFM-$TRAVIS_TAG-win32-ia32.zip HuxleyFM-win32-ia32)
-	(cd dist; zip -r HuxleyFM-$TRAVIS_TAG-win32-x64.zip HuxleyFM-win32-x64)
+  if [[ "$WINE" == "true" ]]; then
+    npm run-script build-windows-wine
+    (cd dist; zip -r HuxleyFM-$TRAVIS_TAG-win32-ia32.zip HuxleyFM-win32-ia32)
+    (cd dist; zip -r HuxleyFM-$TRAVIS_TAG-win32-x64.zip HuxleyFM-win32-x64)
+  else
+    npm run-script build-linux
+    (cd dist; tar zcvf HuxleyFM-$TRAVIS_TAG-linux-ia32.tar.gz HuxleyFM-linux-ia32)
+    (cd dist; tar zcvf HuxleyFM-$TRAVIS_TAG-linux-x64.tar.gz HuxleyFM-linux-x64)
+  fi
 fi
