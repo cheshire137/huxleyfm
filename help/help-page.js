f9c0a7baf4b821a6629b4aedb30c91245bacc4f8
Sarah Vessels <cheshire137@github.com>
2016-06-04 19:05:10 -0500

    Add link to report bugs to help page

--- help/help-page.js
+++ help/help-page.js
@@ -1,13 +1,39 @@
 const Eventful = require('../models/eventful');
+const shell = require('electron').shell;
+const Config = require('../config.json');
+
+const __bind = function(fn, me) {
+  return function() {
+    return fn.apply(me, arguments);
+  };
+};
 
 class HelpPage extends Eventful {
   constructor() {
     console.debug('help page init');
     super();
+    this.reportBug = __bind(this.reportBug, this);
+    this.findElements();
+    this.listenForBugReports();
+  }
+
+  findElements() {
+    this.bugReportLink = document.getElementById('bug-report-link');
+  }
+
+  listenForBugReports() {
+    this.bugReportLink.addEventListener('click', this.reportBug);
+  }
+
+  reportBug(event) {
+    event.preventDefault();
+    event.target.blur();
+    shell.openExternal(Config.new_bug_report_url);
   }
 
   removeListeners() {
     console.debug('unbinding help page listeners');
+    this.bugReportLink.removeEventListener('click', this.reportBug);
   }
 }
 
